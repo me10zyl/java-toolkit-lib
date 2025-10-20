2c6c7bca05566cfa423bc7a39e045548d9172afd
zengyl <me10zyl@qq.com>
2025-10-20 13:59:38 +0800

    .change name

--- toolkit-core/src/main/java/toolkit/enc/advice/RestReturnWrapperHandler.java
+++ toolkit-core/src/main/java/toolkit/enc/advice/RestReturnWrapperHandler.java
@@ -12,6 +12,7 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 import toolkit.enc.dto.EncryptAlogritmEnum;
@@ -26,6 +27,7 @@ import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @ControllerAdvice
+@Component("EncryptRestReturnWrapperHandler")
 public class RestReturnWrapperHandler implements
         ResponseBodyAdvice<Object> {
 
